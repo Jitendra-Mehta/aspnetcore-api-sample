31e78850dd93c4dec5183790f276cba5ebf6a070
jitendra@quolam.com <jitendra@quolam.com>
2020-09-12 00:19:00 +0530

    added image helpers

--- Helpers/ImageHelpers.cs
+++ Helpers/ImageHelpers.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
+using Microsoft.AspNetCore.Http;
+using System.Configuration;
+using System.IO; 
 namespace aspnetcore_api_sample.Helpers
 {
     public class ImageHelpers
     {
+        //string ImagePath = ConfigurationManager.AppSettings["ImagePath"].ToString();
+        //public string UploadAlternativeImages(IFormFile File)
+        //{
+        //    if (File != null)
+        //    {
+        //        string FileName = System.IO.Path.GetFileName(File.FileName);
+        //        string Extention = Path.GetExtension(File.FileName);
 
+        //        string pathString = System.IO.Path.Combine(ImagePath);
+
+        //        if (!Directory.Exists(pathString))
+        //        {
+        //            Directory.CreateDirectory(Server.MapPath(pathString));
+
+        //        }
+
+        //        string finalpath = pathString + FileName.Trim().Replace(" ", "");
+        //        File.SaveAs(HttpContext.Current.Server.MapPath(finalpath));
+
+        //        return finalpath;
+        //    }
+        //    else
+        //    {
+        //        return null;
+        //    }
+        //}
     }
 }
